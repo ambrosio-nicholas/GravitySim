badb5451436062acf9a850addf3e7f3980e33c58
Pizzaboss43 <167825791+ambrosio-nicholas@users.noreply.github.com>
2024-05-12 18:58:13 -0500

    GravitySim V 0.06.1
    Trying to get moons to work.

--- Assets/Attraction_Force.cs
+++ Assets/Attraction_Force.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class Attraction_Force : MonoBehaviour
 {
     public GameObject[] planets;
+    public GameObject[] moons;
     public GameObject star;
     public Rigidbody2D planetRB;
     private float m1;
     const float G = 0.0667f;
 
     private TrailRenderer trail;
-    [SerializeField] Vector2 initialVelocity = new Vector2(0.0f,0.0f);
+    [SerializeField] public Vector2 initialVelocity = new Vector2(0.0f,0.0f);
     [SerializeField] bool specifyInitialVelocity = false;
 
     private void Start()
     {
         //Find all of the planets and their rigidbodies
         planets = GameObject.FindGameObjectsWithTag("planet");
+        moons = GameObject.FindGameObjectsWithTag("moon");
         trail = gameObject.GetComponent<TrailRenderer>();
         star = GameObject.FindGameObjectWithTag("star");
         planetRB = gameObject.GetComponent<Rigidbody2D>();
         m1 = planetRB.mass;
 
         PlanetInitialVelocity();
+        MoonInitialVelocity();
         SetTrail();
     }
 
@@ -67,12 +70,83 @@ public class Attraction_Force : MonoBehaviour
             }
 
         }
+
+        //for each moon that's affecting it
+        for (int i = 0; i < moons.Length; i++)
+        {
+            if(moons[i] != gameObject)
+            {
+                Rigidbody2D otherMoon = moons[i].GetComponent<Rigidbody2D>();
+                float m2 = otherMoon.mass;
+                float distance = Vector2.Distance(planetRB.transform.position, otherMoon.transform.position);
+
+                //calculate force
+                float force = (G * (m1 * m2)) / (distance * distance);
+                Vector2 direction = otherMoon.transform.position - planetRB.transform.position;
+                Vector2 forceWithDirection = (force * (direction / direction.magnitude)) / planetRB.mass;
+
+                //apply force
+                planetRB.velocity += forceWithDirection * Time.deltaTime;
+            }
+        }
     }
 
-    
+    private void MoonInitialVelocity()
+    {
+        if (!specifyInitialVelocity && gameObject.tag == "moon")
+        {
+            //Find which planet is closest to the moon
+            float leastDistance = 999999f;
+            GameObject parentPlanet = gameObject;
+
+            for(int i = 0; i < planets.Length; i++)
+            {
+                float distance = Vector2.Distance(planetRB.transform.position, planets[i].GetComponent<Rigidbody2D>().transform.position);
+                if (distance < leastDistance)
+                {
+                    leastDistance = distance;
+                    parentPlanet = planets[i];
+                }
+            }
+
+            //Find Orbital Velocity around planet
+            float planetDistance = Vector2.Distance(planetRB.transform.position, parentPlanet.GetComponent<Rigidbody2D>().transform.position);
+            float planetMass = parentPlanet.GetComponent<Rigidbody2D>().mass;
+
+            //Find tangent to star
+            Vector2 direction = (planetRB.transform.position - parentPlanet.GetComponent<Rigidbody2D>().transform.position) / planetDistance;
+            float temp = direction.x;
+            direction.x = direction.y;
+            direction.y = -1 * temp;
+
+            //Find Orbital Velocity
+            initialVelocity = new Vector2(Mathf.Sqrt(G * planetMass / planetDistance), 0);
+            print("initialvelocity: " + initialVelocity);
+
+            //Make it tangential to star
+            initialVelocity.y = initialVelocity.x * direction.y;
+            initialVelocity.x *= direction.x;
+
+            //Get that planet's orbital velocity (after one frame)
+            float time = 0f;
+            Vector2 planetOrbitalVelocity = parentPlanet.GetComponent<Rigidbody2D>().velocity; //!!!!!!!!!!!!!!!!!!!!!!!!!!! Need better way to get parent planet's orbital velocity!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            while (time < Time.unscaledDeltaTime)
+            {
+                time += Time.unscaledDeltaTime;
+            }
+
+            print("parent planet's orbital velocity is: " + planetOrbitalVelocity);
+            initialVelocity += planetOrbitalVelocity;
+            print("moons initial velocity is: " + initialVelocity);
+
+            //Apply Orbital Velocity
+            planetRB.velocity = initialVelocity;
+        }
+    }
+
     private void PlanetInitialVelocity()
     {
-        if (star && !specifyInitialVelocity)
+        if (star && !specifyInitialVelocity && gameObject.tag == "planet")
         {
             float distance = Vector2.Distance(planetRB.transform.position, star.GetComponent<Rigidbody2D>().transform.position);
             float starMass = star.GetComponent<Rigidbody2D>().mass;
